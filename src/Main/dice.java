8ed782ce2b16da742dfcb8265514c0184602e4f8
Boersma <helenebo@hotmail.fr>
2019-08-29 20:12:35 +0200

    bite

--- src/Main/dice.java
+++ src/Main/dice.java
@@ -1,7 +1,7 @@
 package Main;
 import java.util.* ;
 
-public class random {
+public class dice {
     public static int random(int x){
         Random r = new Random();
         int a = r.nextInt(x)+1;
--- src/Main/main.java
+++ src/Main/main.java
@@ -3,7 +3,7 @@ package Main;
 
 public class main {
     public static void main(String[] args) {
-        int x = random.random(6);
+        int x = dice.random(6);
         System.out.println("la valeur du dé est : " + x);
         System.out.println("test");
         System.out.println("test 2");
