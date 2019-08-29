444c5aba2c2bc217cd72f0ddcd06ff8655e83e5b
Antoine <antoine.didier@efrei.net>
2019-08-29 13:17:41 -0500

    Revert "suppression des tests à la con"
    This reverts commit a26b67cf50b71eb4e3dd60324fd7e0980d250a41.

--- src/Main/main.java
+++ src/Main/main.java
@@ -5,5 +5,7 @@ public class main {
     public static void main(String[] args) {
         int x = dice.random(6);
         System.out.println("la valeur du dé est : " + x);
+        System.out.println("test");
+        System.out.println("test 2");
     }
 }
