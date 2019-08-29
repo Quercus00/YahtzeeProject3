bf1cde094afd3fe7adec440cb8d8ce3c9d694464
Antoine <antoine.didier@efrei.net>
2019-08-29 13:33:18 -0500

    Fct roll dice

--- src/Main/dice.java
+++ src/Main/dice.java
@@ -7,5 +7,18 @@ public class dice {
         int a = r.nextInt(x)+1;
         return a;
     }
+
+    public static void rollDice(){
+        int de1 = dice.random(6);
+        int de2 = dice.random(6);
+        int de3 = dice.random(6);
+        int de4 = dice.random(6);
+        int de5 = dice.random(6);
+        System.out.println("la valeur du dé est : " + de1);
+        System.out.println("la valeur du dé est : " + de2);
+        System.out.println("la valeur du dé est : " + de3);
+        System.out.println("la valeur du dé est : " + de4);
+        System.out.println("la valeur du dé est : " + de5);
+    }
 }
 
--- src/Main/main.java
+++ src/Main/main.java
@@ -3,16 +3,6 @@ package Main;
 
 public class main {
     public static void main(String[] args) {
-        int de1 = dice.random(6);
-        int de2 = dice.random(6);
-        int de3 = dice.random(6);
-        int de4 = dice.random(6);
-        int de5 = dice.random(6);
-        System.out.println("la valeur du dé est : " + de1);
-        System.out.println("la valeur du dé est : " + de2);
-        System.out.println("la valeur du dé est : " + de3);
-        System.out.println("la valeur du dé est : " + de4);
-        System.out.println("la valeur du dé est : " + de5);
-
+        dice.rollDice();
     }
 }
