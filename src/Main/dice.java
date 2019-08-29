bebc45b95edee6d309a968930fef21599f8c05a9
alban <alban.rambert@efrei.net>
2019-08-29 17:23:19 -0400

    CA MARCHE

--- src/Main/dice.java
+++ src/Main/dice.java
@@ -14,16 +14,6 @@ public class dice {
         return d;
     }
 
-    public static void rollDice(){
-
-        int de1 = dice.random(6);
-        int de2 = dice.random(6);
-        int de3 = dice.random(6);
-        int de4 = dice.random(6);
-        int de5 = dice.random(6);
-        System.out.println("the dices scores are : " + de1+"; "+de2 +"; "+  de3 +"; "+ de4+"; " + de5);
-
-    }
 
 }
 
