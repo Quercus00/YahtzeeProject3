8b54054bd32b4537d0865940b83e5ef582636c28
Antoine <antoine.didier@efrei.net>
2019-08-29 13:30:45 -0500

    5 dés créés

--- src/Main/dice.java
+++ src/Main/dice.java
@@ -8,3 +8,4 @@ public class dice {
         return a;
     }
 }
+
--- src/Main/main.java
+++ src/Main/main.java
@@ -3,11 +3,16 @@ package Main;
 
 public class main {
     public static void main(String[] args) {
-        int x = dice.random(6);
-        System.out.println("la valeur du dé est : " + x);
-        System.out.println("test");
-        System.out.println("test 2");
-        System.out.println("aaaa");
-        System.out.println("dieu nous aide");
+        int de1 = dice.random(6);
+        int de2 = dice.random(6);
+        int de3 = dice.random(6);
+        int de4 = dice.random(6);
+        int de5 = dice.random(6);
+        System.out.println("la valeur du dé est : " + de1);
+        System.out.println("la valeur du dé est : " + de2);
+        System.out.println("la valeur du dé est : " + de3);
+        System.out.println("la valeur du dé est : " + de4);
+        System.out.println("la valeur du dé est : " + de5);
+
     }
 }
