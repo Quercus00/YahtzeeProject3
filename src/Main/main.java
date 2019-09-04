991727628ff202d62dd297e66915a32f65c84a5c
Antoine <antoine.didier@efrei.net>
2019-09-04 19:22:21 -0400

    Maj du main

--- src/Main/main.java
+++ src/Main/main.java
@@ -80,7 +80,48 @@ public class main {
             System.out.println("YAHTZEE");
         }*/
 
-        
+        int players = 0; // default game is for one player
+        do{
+            System.out.println("How many players ? (5 players maximum)\n---> ");
+            Scanner choice = new Scanner(System.in);
+            players = choice.nextInt();
+        }while(players < 1 || players>5);
+
+
+        //intialisation of the data and the databoolean :
+        int[][] data;
+        boolean[][] databool;
+        data = memory.data(players);
+        databool = memory.dataBool(players);
+
+
+        boolean stop = false;
+        while(!stop){ // while the score is not complete, we continue to play
+
+
+            //Play the turn of each player
+            for(int i=1; i<=players ; i++){
+                System.out.println("******* Player " + i + " *********");
+            }
+
+            // test if the score is complete : we check the score sheet of the last player
+            stop = true;
+            for(int i=0 ; i<13; i++){
+                if (!databool[players-1][i]) {
+                    stop = false;
+                    break;
+                }
+            }
+
+
+
+
+        }
+
+
+
+
+
 
 
 
